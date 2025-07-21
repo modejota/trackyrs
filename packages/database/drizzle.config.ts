6ea57424780f90254ea32ad85ea9085cb414d1bf
modejota <dev@modejota.com>
2025-07-21 18:48:45 +0200

    build: change module in tsconfig

--- packages/database/drizzle.config.ts
+++ packages/database/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { config } from "dotenv";
 import { defineConfig } from "drizzle-kit";
 
-config({ path: [".env.local"], override: true });
+config({ path: ["../../.env.local"], override: true });
 
 export default defineConfig({
 	dialect: "postgresql",
