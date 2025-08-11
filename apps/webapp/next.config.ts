d54d4a2ee555b1956c434e995c5b8a7d12a48710
modejota <dev@modejota.com>
2025-08-11 19:33:51 +0200

    chore: allow images from myanimelist CDN

--- apps/webapp/next.config.ts
+++ apps/webapp/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	transpilePackages: ["@trackyrs/ui"],
+	images: {
+		remotePatterns: [new URL("https://cdn.myanimelist.net/**")],
+	},
 };
 
 export default nextConfig;
