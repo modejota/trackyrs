bbbb0c5eabd77c2c8d8ed8612df76c771a28b8a3
modejota <dev@modejota.com>
2025-08-25 18:48:10 +0200

    chore: profile page

---
+++ apps/webapp/app/profile/[username]/page.tsx
@@ -0,0 +1,10 @@
+import ClientProfile from "@/app/profile/[username]/client";
+
+interface ProfilePageProps {
+	params: Promise<{ username: string }>;
+}
+
+export default async function ProfilePage({ params }: ProfilePageProps) {
+	const { username } = await params;
+	return <ClientProfile username={username} />;
+}
