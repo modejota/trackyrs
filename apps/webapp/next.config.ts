3ada9f177b7ef9cbaa35d3f909225f5e2cc7d2f9
modejota <dev@modejota.com>
2025-08-30 16:36:08 +0200

    feat: setting docker-compose for production | set START options for production

--- apps/webapp/next.config.ts
+++ apps/webapp/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	transpilePackages: ["@trackyrs/ui", "@trackyrs/utils", "@trackyrs/database"],
 	images: {
+		unoptimized: true,
 		remotePatterns: [new URL("https://cdn.myanimelist.net/**")],
 	},
 };
